86f294435adb632fa36ee58fb306a4a945b0cff6
Matt McElheny <mattmc3@gmail.com>
2018-08-30 10:22:14 -0400

    :fireworks: Make install script better

--- install.sh
+++ install.sh
@@ -1,28 +1,27 @@
 #!/usr/bin/env bash
 
-taskpaper_dir="$HOME/Library/Containers/com.hogbaysoftware.TaskPaper3/Data/Library/Application Support/TaskPaper/StyleSheets"
-taskpaper_dir_appstore="$HOME/Library/Application Support/TaskPaper/StyleSheets"
-gh_url="https://raw.githubusercontent.com/mattmc3/taskpaper-themes/master"
-found=
+function main() {
+    local taskpaper_dirs=(
+        "$HOME/Library/Application Support/TaskPaper"
+        "$HOME/Library/Containers/com.hogbaysoftware.TaskPaper3/Data/Library/Application Support/TaskPaper"
+    )
+    local gh_url="https://raw.githubusercontent.com/mattmc3/taskpaper-themes/master"
+    local found=
 
-if [[ -d "$taskpaper_dir" ]]
-then
-    echo "Found TaskPaper (non App Store edition).  Installing themes..."
-    found=1
-    curl -fsSL "$gh_url/jellybeans.less" > "$taskpaper_dir/jellybeans.less"
-    curl -fsSL "$gh_url/tomorrow-night.less" > "$taskpaper_dir/tomorrow-night.less"
-fi
+    for d in "${taskpaper_dirs[@]}"
+    do
+        if [[ -d "$d" ]]; then
+            found=1
+            echo "Installing themes..."
+            curl -fsSL "$gh_url/jellybeans.less" > "$d/jellybeans.less"
+            curl -fsSL "$gh_url/tomorrow-night.less" > "$d/tomorrow-night.less"
+        fi
+    done
 
-if [[ -d "$taskpaper_dir_appstore" ]]
-then
-    echo "Found TaskPaper (App Store edition).  Installing themes..."
-    found=1
-    curl -fsSL "$gh_url/jellybeans.less" > "$taskpaper_dir_appstore/jellybeans.less"
-    curl -fsSL "$gh_url/tomorrow-night.less" > "$taskpaper_dir_appstore/tomorrow-night.less"
-fi
-
-if [[ $found != 1 ]]
-then
-    echo "TaskPaper cannot be found. Aborting install." 1>&2
-    exit 1
-fi
+    if [[ $found != 1 ]]
+    then
+        echo "TaskPaper not found. Aborting." 1>&2
+        exit 1
+    fi
+}
+main
